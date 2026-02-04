1f8213bdb0dfc45bd7668bfe49dc1ac53306664e
Nix177 <nicolas.tuor@bluewin.ch>
2026-02-04 20:17:11 +0100

    fix: Update base path casing to match GitHub repo name

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/micro-mentor/",
+    base: "/Micro-mentor/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
